dadd0e57f0d84733dc47b15e2098319c86c43048
Sumon Hossain <136088595+SumonHossainJs@users.noreply.github.com>
2023-10-09 19:19:51 +0300

    add Global styles with variable and responsive scss file

--- admin-dashboard/dboard/src/App.jsx
+++ admin-dashboard/dboard/src/App.jsx
@@ -1,12 +1,71 @@
 import React from 'react';
 
 import {RouterProvider,createBrowserRouter, Outlet} from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
+import "./styles/global.scss";
+import { Users, Login, User, Product, Products, Home } from './pages/ExportPages';
+import { Navbar, Footer, Menu} from './componets/ExportCom';
+
+
+const queryClient = new QueryClient();
 
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const Layout = () =>{
+    return(
+      <div className="main">
+        <Navbar/>
+        <div className="container">
+          <div className="menuContainer">
+            <Menu/>
+          </div>
+          <div className="contentContainer">
+            <QueryClientProvider client={queryClient}>
+              <Outlet/>
+            </QueryClientProvider>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element:<Layout/>,
+      children:[
+        {
+          path:'/',
+          element:<Home/>
+        },
+        {
+          path:'/users',
+          element:<Users/>
+        },
+        {
+          path:'/products',
+          element:<Products/>
+        },
+        {
+          path:'/user/:id',
+          element:<User/>
+        },
+        {
+          path:'/product/:id',
+          element:<Product/>
+        },
+       
+      ]
+    }, {
+      path:"/login",
+      element:<Login/>
+    }
+  ])
+  return <RouterProvider router={router}/>
 }
 
 export default App
