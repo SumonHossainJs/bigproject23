b1675981696609e3d6bb02f466f21267f0eaab8e
Sumon Hossain <136088595+SumonHossainJs@users.noreply.github.com>
2023-10-20 18:22:40 +0300

    20 10 23 1st commit

---
+++ admin-dashboard/dboard/public/index.js
@@ -0,0 +1,10 @@
+import Logo from '/logo.svg';
+import SettingIcon from '/settings.svg';
+import notification from '/notifications.svg';
+import appIcon from '/app.svg';
+import expandIcon from '/expand.svg';
+import searchIcon from '/search.svg'
+
+export {
+    Logo, SettingIcon, notification, appIcon, expandIcon,searchIcon
+}
--- admin-dashboard/dboard/src/componets/navbar/Navbar.jsx
+++ admin-dashboard/dboard/src/componets/navbar/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.scss';
+import { Logo, notification, appIcon, SettingIcon, expandIcon, searchIcon, } from '../../../public/index';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="logo">
+        <img src={Logo} alt="Logo Image" />
+        <span>ADashoard</span>
+      </div>
+      <div className="icons">
+        <img src={searchIcon} alt=""className='icon' />
+        <img src={appIcon} alt="" className="icon" />
+        <img src={expandIcon} alt="" className="icon" />
+        <div className="notification">
+          <img src={notification} alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="https://images.unsplash.com/photo-1693924198149-e14ff98ed3f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" alt="" />
+          <span>Jane</span>
+        </div>
+        <img src={SettingIcon} className='icon' alt="" />
+      </div>
+    </div>
   )
 }
 
