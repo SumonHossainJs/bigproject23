dd35ea6ea4562ff0b6431b098a43baca19e04136
Sumon Hossain <136088595+SumonHossainJs@users.noreply.github.com>
2023-10-28 21:10:16 +0300

    al hamdulilah 28 10 23

--- admin-dashboard/tdboard/src/components/PieChartBox/PieChartBox.tsx
+++ admin-dashboard/tdboard/src/components/PieChartBox/PieChartBox.tsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import  "./PieChartBox.scss"
+
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+
+const Data = [
+  {
+    name:'Mobile', value:400, color:"#a9f907"
+  },
+  {
+    name:'Laptop', value:900, color:"#fa0707"
+  },
+  {
+    name:'Desktop', value:300, color:"#08f8f4"
+  },
+  {
+    name:'Tablet', value:500, color:"#2e05f7"
+  },
+]
+
 
 const PieChartBox = () => {
   return (
-    <div>PieChartBox</div>
+    <div className="Piechatbox">
+      <h1>Source By Leads</h1>
+      <div className="chart">
+        <ResponsiveContainer width="99%" height={300}>
+            <PieChart>
+              <Tooltip
+              contentStyle={{background:'white', borderRadius:'50px'}}/>
+
+              <Pie
+              data={Data}
+              dataKey="value"
+              innerRadius={"70%"}
+              outerRadius={'90%'}
+              paddingAngle={5}
+              >
+
+                {Data.map((item)=>(
+                  <Cell key={item.name} fill={item.color}/>
+                ))}
+              </Pie>
+              
+            </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="options">
+        {Data.map((item)=>(
+          <div className="option" key={item.name}>
+            <div className="title">
+              <div className="dot" style={{backgroundColor:item.color}}>
+              </div>
+                <span>{item.name}</span>
+            </div>
+              <span>{item.value}</span>
+          </div>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
--- admin-dashboard/tdboard/src/components/chartBox/ChartBox.tsx
+++ admin-dashboard/tdboard/src/components/chartBox/ChartBox.tsx
@@ -1,5 +1,6 @@
-import "./ChartBox.scss";
-
+import { Link } from "react-router-dom";
+import "./chartBox.scss";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
 
 type Props = {
   color: string;
@@ -11,10 +12,50 @@ type Props = {
   chartData: object[];
 };
 
-const ChartBox = (props:Props) => {
+const ChartBox = (props: Props) => {
   return (
-    <div>ChartBox</div>
-  )
-}
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          <img src={props.icon} alt="" />
+          <span>{props.title}</span>
+        </div>
+        <h1>{props.number}</h1>
+        <Link to="/" style={{ color: props.color }}>
+          View all
+        </Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 70 }}
+              />
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke={props.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {props.percentage}%
+          </span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ChartBox
+export default ChartBox;
