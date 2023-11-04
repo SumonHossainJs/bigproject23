7c7f3a62df50f182db516a5fc70369c44594a4af
Sumon Hossain <136088595+SumonHossainJs@users.noreply.github.com>
2023-11-04 18:20:43 +0300

    Al hamdulilah subhan allah

--- fullstack-video-sharing-app/client/src/App.jsx
+++ fullstack-video-sharing-app/client/src/App.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home, Login, Video } from "./Pages";
+import { Navbar, Menu } from "./Components";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    );
+  };
 
-export default App
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/video/:id",
+        element: <Video />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+return <RouterProvider router={router}/>
+};
+export default App;
