caa8c5f8564ae42f74659197d1f2edfee15c6e6a
Sumon Hossain <136088595+SumonHossainJs@users.noreply.github.com>
2023-11-09 15:56:52 +0300

    add update route

---
+++ fullstack-video-sharing-app/server/routes/user.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import { Update,deleteUser, getUser, subscribe, unsubscribe, like, disLike  } from '../controllers/user.control.js';
+import {verifyToken} from '../verifyToken.js';
+
+const router = express.Router();
+
+// update user
+router.put("/update/:id", verifyToken, Update);
+
