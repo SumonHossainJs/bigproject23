1b301264397a8896f2a3c94db8b26627b6b864e9
Sumon Hossain <136088595+SumonHossainJs@users.noreply.github.com>
2023-11-09 16:15:45 +0300

    video route done

--- fullstack-video-sharing-app/server/routes/video.route.js
+++ fullstack-video-sharing-app/server/routes/video.route.js
@@ -5,7 +5,17 @@ import {verifyToken} from '../verifyToken.js';
 
 const router = express.Router();
 
+// add video
 
+router.post("/addVideo", verifyToken, addVideo);
+router.put("/video/:id", verifyToken, updateVideo);
+router.delete("/video/:id", verifyToken, deleteVideo);
+router.get("/find/:id", getVideo);
+router.put("/view", addView);
+router.get("/random", randomVideo);
+router.get("/tag", getbyTag);
+router.get("/sub", verifyToken, sub);
+router.get("/search", search);
 
 export default router;
 
