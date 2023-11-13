427b4b3ffa2e161ef8e199eba19cb9b6e24e6a03
Sumon Hossain <136088595+SumonHossainJs@users.noreply.github.com>
2023-11-13 21:56:00 +0300

    complete user slice

--- fullstack-video-sharing-app/client/src/Redux/userSlice.js
+++ fullstack-video-sharing-app/client/src/Redux/userSlice.js
@@ -5,14 +5,14 @@ const intialState = {
     error:false,
 }
 
-export const userSlice = {
+export const userSlice = createSlice({
     name: 'user',
     intialState, 
     reducers:{
         loginStart: (state) =>{
             state.loading = true;
         },
-        loginSuccess: (state) =>{
+        loginSuccess: (state, action) =>{
             state.loading = false;
             state.currentUser = action.payload;
         },
@@ -25,5 +25,21 @@ export const userSlice = {
             state.loading = false;
             state.error = false;
         },
+        subscription:(state, action) =>{
+                if(state.currentUser.subscribedUsers.includes(action.payload)){
+                    state.currentUser.subscribedUsers.splice(
+                        state.currentUser.subscribedUsers.findIndex(
+                            (channelId) => channelId === action.payload
+                        ),
+                        1
+                    )
+                }else{
+                    state.currentUser.subscribedUsers.push(action.payload);
+                }
+        }
     }
-}
+})
+
+export const { loginStart, loginSuccess, loginFailure,logout, subscription} = userSlice.actions
+
+export default userSlice.reducer;
