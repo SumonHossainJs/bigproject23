dbeeda4654a3c510ce0a1bfc7b69818be1a4825f
Sumon Hossain <136088595+SumonHossainJs@users.noreply.github.com>
2023-11-14 19:55:24 +0300

    al hamdulilah Subhan allah

--- fullstack-video-sharing-app/client/src/Redux/videoSlice.js
+++ fullstack-video-sharing-app/client/src/Redux/videoSlice.js
@@ -1,53 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const intialState = {
-    currentVideo: null,
-    loading: false, 
-    erorr :false,
-}
+const initialState = {
+  currentVideo: null,
+  loading: false,
+  error: false,
+};
 
-const videoSlice = createSlice({
-    name:'video',
-    intialState,
-    reducers:{
-        fetchStart:(state)=> {
-            state.loading = true;
-        },
-        fetchSuccess:(state,action)=>{
-            state.loading= false;
-            state.currentVideo = action.payload;
-        },
-        fetchFailure:(state)=>{
-            state.loading = false,
-            state.currentVideo = null;
-            state.error = true;
-        },
-        like:(state,action) =>{
-            if(!state.currentVideo.likes.includes(action.payload)){
-                state.currentVideo.likes.push(action.payload);
-                state.currentVideo.dislike.splice(
-                    state.currentVideo.dislikes.findIndex(
-                        (userId) => userId === action.payload
-                    ),
-                    1
-                )
-            }
-        },
-        dislike:(state,action)=>{
-            if(!state.currentVideo.dislikes.includes(action.payload)){
-                state.currentVideo.dislikes.push(action.payload);
-                state.currentVideo.likes.splice(
-                    state.currentVideo.likes.findIndex(
-                        (userId) => userId === action.payload
-                    ),
-                    1
-                )
-            }
+export const videoSlice = createSlice({
+  name: "video",
+  initialState,
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+    },
+    fetchSuccess: (state, action) => {
+      state.loading = false;
+      state.currentVideo = action.payload;
+    },
+    fetchFailure: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+    like: (state, action) => {
+      if (!state.currentVideo.likes.includes(action.payload)) {
+        state.currentVideo.likes.push(action.payload);
+        state.currentVideo.dislikes.splice(
+          state.currentVideo.dislikes.findIndex(
+            (userId) => userId === action.payload
+          ),
+          1
+        );
+      }
+    },
+    dislike: (state, action) => {
+      if (!state.currentVideo.dislikes.includes(action.payload)) {
+        state.currentVideo.dislikes.push(action.payload);
+        state.currentVideo.likes.splice(
+          state.currentVideo.likes.findIndex(
+            (userId) => userId === action.payload
+          ),
+          1
+        );
+      }
+    },
+  },
+});
 
-        }
+export const { fetchStart, fetchSuccess, fetchFailure, like, dislike } =
+  videoSlice.actions;
 
-    }
-})
-
-export const {fetchFailure, fetchStart, fetchSuccess, like, dislike} = videoSlice.actions;
 export default videoSlice.reducer;
